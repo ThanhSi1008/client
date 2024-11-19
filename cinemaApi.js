040b74185f6c4fde607af28ab5d6a60997b21a94
System Administrator <root@xishnaht108.local>
2024-11-19 14:55:43 +0700

    nothing

--- cinemaApi.js
+++ cinemaApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://thankyouandyou.vercel.app',
+  baseURL: 'https://cinema-server-mongodb.vercel.app',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
